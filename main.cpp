a49841023199f0a77c67f3e25d67d3f515e7918f
aqariio <aquario.mw@gmail.com>
2023-11-21 15:55:19 -0500

    Merge with recent commit

--- main.cpp
+++ main.cpp
@@ -13,20 +13,21 @@ using namespace std;
 
 ofstream outputSaveStream;
 ifstream inputReadStream;
-string currentVersion = "0.3.1";
+string currentVersion = "0.4";
 const static vector<string> COMMANDS = {"showCommands", "version", "add", "exit", "clear", "display", "remove"};
-const static int WIDTH = 60;
+const int WIDTH = 60;
 
 class Task {
 public:
     string name;
+    string date;
 };
 
 vector<Task> tasks;
 
 class Program {
 private:
-    static void printTopRow() {
+    void printTopRow() {
         cout << "\n";
         for (int i = 0; i < WIDTH; i++) {
             cout << "-";
@@ -34,14 +35,14 @@ private:
         cout << "\n";
     }
 
-    static void printBottomRow() {
+    void printBottomRow() {
         for (int i = 0; i < WIDTH; i++) {
             cout << "-";
         }
         cout << "\n\n";
     }
 
-    static void printf(const string& text) {
+    void printf(string text) {
         int size = text.size();
         int width = WIDTH;
         width -= 3;
@@ -56,6 +57,59 @@ private:
     }
 
 public:
+    void checkDate(string date) {
+        if (date.size() != 10) {
+            cout << "Date must be in the format of YYYY-MM-DD.\n";
+            showMenu();
+            return;
+        }
+        if (date[4] != '-' || date[7] != '-') {
+            cout << "Date must be in the format of YYYY-MM-DD.\n";
+            showMenu();
+            return;
+        }
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int day = stoi(date.substr(8, 2));
+        if (year < 2023) {
+            cout << "Year must be greater than or equal to 2023.\n";
+            showMenu();
+            return;
+        }
+        if (month < 1 || month > 12) {
+            cout << "Month must be between 1 and 12.\n";
+            showMenu();
+            return;
+        }
+        if (day < 1 || day > 31) {
+            cout << "Day must be between 1 and 31.\n";
+            showMenu();
+            return;
+        }
+        if (month == 2) {
+            if (year % 4 == 0) {
+                if (day > 29) {
+                    cout << "Day must be between 1 and 29.\n";
+                    showMenu();
+                    return;
+                }
+            } else {
+                if (day > 28) {
+                    cout << "Day must be between 1 and 28.\n";
+                    showMenu();
+                    return;
+                }
+            }
+        }
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (day > 30) {
+                cout << "Day must be between 1 and 30.\n";
+                showMenu();
+                return;
+            }
+        }
+    }
+
     void showMenu() {
         cout << "Execute a command here (showCommands to show COMMANDS): ";
         string command;
@@ -89,8 +143,8 @@ public:
 
     void showCommands() {
         printTopRow();
-        printf("Here are the list of COMMANDS:");
-        printf("showCommands -> show all COMMANDS available (0.1)");
+        printf("Here are the list of commands:");
+        printf("showCommands -> show all commands available (0.1)");
         printf("version -> show version of program (0.1)");
         printf("add -> add a task (0.2)");
         printf("exit -> exit program (0.2)");
@@ -116,6 +170,11 @@ public:
         string name;
         getline(cin, name);
         task.name = name;
+        cout << "Enter the date of the task (YYYY-MM-DD): ";
+        string date;
+        getline(cin, date);
+        checkDate(date);
+        task.date = date;
         tasks.push_back(task);
         outputSaveStream << task.name << "\n";
         cout << "Task added.\n";
@@ -136,7 +195,6 @@ public:
         printTopRow();
         cout << "Are you sure you want to clear all tasks? (y/n): ";
         string answer;
-        cin.ignore();
         getline(cin, answer);
         if (answer == "y") {
             outputSaveStream.close();
@@ -178,10 +236,10 @@ public:
             return;
         }
         printf("Here are the list of tasks:");
-        printf("ID | Task Name                     ");
+        printf("ID | Task Name | Due Date");
         int id = 1;
         for (Task task : tasks) {
-            printf(to_string(id) + " | " + task.name);
+            printf(to_string(id) + " | " + task.name + " | " + task.date);
             id++;
         }
         printBottomRow();
@@ -192,7 +250,7 @@ public:
         printTopRow();
         string sid;
         cout << "Enter the ID of the task you want to remove: ";
-        cin >> sid;
+        getline(cin, sid);
         if (!all_of(sid.begin(), sid.end(), ::isdigit)) {
             cout << "ID must be a number.\n";
             printBottomRow();
@@ -208,7 +266,7 @@ public:
         }
         cout << "Are you sure you want to remove this task? (y/n): ";
         string answer;
-        cin >> answer;
+        getline(cin, answer);
         if (answer == "y") {
             tasks.erase(tasks.begin() + id - 1);
             cout << "Removed.\n";
